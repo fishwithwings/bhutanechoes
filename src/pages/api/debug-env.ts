aa474c741b69510f70862ee9b8de2f4b59218452
Thinley Wangchuk <thinley@MacBook-Pro.local>
2026-06-15 15:12:29 +0700

    Debug env: show key prefixes

--- src/pages/api/debug-env.ts
+++ src/pages/api/debug-env.ts
@@ -4,10 +4,13 @@ export const prerender = false;
 
 export const GET: APIRoute = async ({ locals }) => {
   const env = (locals as any).runtime?.env ?? {};
+  const serviceKey = env.SUPABASE_SERVICE_ROLE_KEY ?? '';
   return new Response(JSON.stringify({
     has_url: !!env.PUBLIC_SUPABASE_URL,
     has_anon: !!env.PUBLIC_SUPABASE_ANON_KEY,
-    has_service: !!env.SUPABASE_SERVICE_ROLE_KEY,
-    keys: Object.keys(env),
+    has_service: !!serviceKey,
+    service_key_prefix: serviceKey.slice(0, 12),
+    service_key_length: serviceKey.length,
+    anon_key_prefix: (env.PUBLIC_SUPABASE_ANON_KEY ?? '').slice(0, 12),
   }, null, 2), { headers: { 'Content-Type': 'application/json' } });
 };
